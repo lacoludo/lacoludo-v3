1fb2cf9af1874e1d8e3a80cc8136c54049e7ce38
LACOUTURE Ludovic <email@lacoludo.com>
2017-05-15 01:49:35 +0200

    bar + circle style

---
+++ dist/js/bar.js
@@ -0,0 +1,410 @@
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+var bar = new ProgressBar.Line(htmlcss, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.9);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(javascript, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(angularjs, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.1);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(php, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.3);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(cms, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(uiux, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.8);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(webindexing, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(english, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.8);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(espanol, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.3);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(francais, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.9);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(telugu, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.2);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(thamizh, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.9);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(htmlcsssmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.9);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(javascriptsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(angularjssmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.1);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(phpsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.3);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(cmssmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(uiuxsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.8);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(webindexingsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(englishsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.8);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(espanolsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.3);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(francaissmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.9);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(telugusmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.2);  // Number from 0.0 to 1.0
+
+var bar = new ProgressBar.Line(thamizhsmall, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 5000,
+  color: '#01D5DA',
+  trailColor: '#eee',
+  trailWidth: 4,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#01D5DA'},
+  to: {color: '#01D5DA'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
+
+bar.animate(0.9);  // Number from 0.0 to 1.0
